7d4b3d019f2b07854dbe8b0bb72c01650d3f2b1e
Yago Cunha <yago.cunha123@gmail.com>
2021-04-16 00:36:39 -0300

    finishing first version of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal'
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./hooks/useTransactions";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionOpen, setIsNewTransactionOpen] = useState(false)
+  
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionOpen(true)
+  }
+
+  function handleCloaseNewTransactionModal() {
+    setIsNewTransactionOpen(false)
+  }
+
   return (
-    <>
-      <Header />
+    <TransactionsProvider>
+      <Header 
+        onOpenNewTransactionModal={handleOpenNewTransactionModal}
+      />
+
       <Dashboard />
+
+      <NewTransactionModal 
+        isOpen={isNewTransactionOpen}
+        onRequestClose={handleCloaseNewTransactionModal}
+      />
+
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
